2969b3ba2585006aa4af7577dfb73e9c2e44e9aa
Nhon Tran <nhonvn2509@gmail.com>
2024-02-29 04:18:08 +0700

    finished all Model UI

--- Pages/LoginPage.cshtml.cs
+++ Pages/LoginPage.cshtml.cs
@@ -1,12 +1,41 @@
+using Microsoft.AspNetCore.Authentication;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.AspNetCore.Mvc.RazorPages;
+using SignalAssigment_2.Model;
+using System.Security.Claims;
 
 namespace SignalAssigment_2.Pages
 {
     public class LoginPageModel : PageModel
     {
+        [BindProperty]
+        public Account account { get; set; }
+
         public void OnGet()
         {
+
+        }
+        public async Task<IActionResult> OnPostAsync()
+        {
+            if (!ModelState.IsValid) return Page();
+            // Verify
+            if(account.UserName == "admin" && account.Password == "admin")
+            {
+                // Create the security context
+                var claims = new List<Claim>
+                {
+                    new Claim(ClaimTypes.Name, "admin"),
+                    new  Claim(ClaimTypes.Email, "admin")
+                };
+                var identity = new ClaimsIdentity(claims, "MyCookieAuth");
+
+                ClaimsPrincipal claimsPrincipal = new ClaimsPrincipal(identity);
+
+                await HttpContext.SignInAsync("MyCookieAuth", claimsPrincipal);
+
+                return RedirectToPage("/Index");
+            }
+            return Page();
         }
     }
 }
--- Program.cs
+++ Program.cs
@@ -15,7 +15,13 @@ namespace SignalAssigment_2
             // Register database
             var connectionString = builder.Configuration.GetConnectionString("DefaultConnection");
             builder.Services.AddDbContext<ApplicationContext>(options => options.UseSqlServer(connectionString));
-
+            //Add authentication
+            builder.Services.AddAuthentication("MyCookieAuth").AddCookie(
+                "MyCookieAuth", options =>
+                {
+                    options.Cookie.Name = "MyCookieAuth";
+                    options.LoginPath = "/Index";
+                });
             var app = builder.Build();
 
             // Configure the HTTP request pipeline.
